112a212686abe8a3efe24ef407e0385aacd46eaa
Anthony McDonald <a@awews.com>
2018-03-29 22:30:42 -0400

    add authForms

---
+++ src/components/Forms/AuthForm.js
@@ -0,0 +1,208 @@
+import React, { Component } from 'react';
+import './AuthForm.css';
+
+class AuthForm extends Component {
+  static defaultProps= {
+    onAuth() {},
+    heading: 'Welcome back.',
+    buttonText: 'Login',
+    errorMessage: undefined
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      profileImageUrl: '',
+      signUp: true
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentWillMount() {
+    let signUp = this.props.showSignUpForm;
+    this.setState({
+      signUp
+    })
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let { signUp, ...user } = { ...this.state };
+    let { showSignUpForm } = this.props;
+    console.log(user, showSignUpForm);
+
+    this.props.onAuth({ ...user });
+
+    this.setState({
+      email: '',
+      username: '',
+      password: '',
+      profileImageUrl: ''
+    });
+  }
+
+  toggleSignUp = () => {
+    let signUp = this.state.signUp;
+    this.setState({
+      signUp: !signUp
+    });
+  }
+
+  render() {
+    const { email, password, profileImageUrl, username } = this.state;
+    const { buttonText, errorMessage, heading, onClose, onShowLoginForm, onShowSignUpForm } = this.props;
+
+    if (!this.props.showSignUpForm) {
+      return (
+        <div
+          className=''>
+          <button
+            type='button'
+            className=''
+            onClick={ onClose }>
+            Close
+          </button>
+          <button
+            type='button'
+            className=''
+            onClick={ onShowSignUpForm }>
+            Sign Up
+          </button>
+          <form onSubmit={ this.handleSubmit }>
+            <h2>{ heading }</h2>
+            { errorMessage ?
+              <div className=''>
+                { errorMessage}
+              </div> :
+              undefined
+            }
+            <label
+              htmlFor='signin-email'>
+              Email
+            </label>
+            <input
+              id='signin-email'
+              key='email'
+              type='text'
+              name='email'
+              className='form-control'
+              autoComplete='off'
+              value={ email }
+              onChange={ this.handleChange } />
+            <label
+              htmlFor='signin-password'>
+              Password
+            </label>
+            <input
+              id='signin-password'
+              key='password'
+              type='password'
+              name='password'
+              className='form-control'
+              autoComplete='off'
+              value={ password }
+              onChange={ this.handleChange } />
+            <button
+              type='submit'
+              className=''>
+              { buttonText }
+            </button>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className=''>
+          <button
+            type='button'
+            className='close-button'
+            onClick={ onClose }>
+            Close
+          </button>
+          <button
+            type='button'
+            className=''
+            onClick={ onShowLoginForm }>
+            Sign In
+          </button>
+          <form
+            onSubmit={ this.handleSubmit }>
+            <h2>{ heading }</h2>
+            { errorMessage ? <div className=''>
+              { errorMessage }</div> :
+              undefined }
+            <label
+              htmlFor='signup-email'>
+              Email
+            </label>
+            <input
+              id='signup-email'
+              key='email'
+              type='text'
+              name='email'
+              className='form-control'
+              autoComplete='off'
+              value={ email }
+              onChange={ this.handleChange } />
+            <label
+              htmlFor='signup-username'>
+              <input
+                id='signup-username'
+                key='username'
+                type='text'
+                name='username'
+                className='form-control'
+                autoComplete='off'
+                value={ username }
+                onChange={ this.handleChange } />
+                Username
+            </label>
+            <label
+              htmlFor='signup-password'>
+              Password
+            </label>
+            <input
+              id='signup-password'
+              key='password'
+              type='password'
+              name='password'
+              className='form-control'
+              autoComplete='off'
+              value={ password }
+              onChange={ this.handleChange } />
+            <label
+              htmlFor='signup-image-url'>
+              Image URL
+            </label>
+            <input
+              id='signup-image-url'
+              key='profileImageUrl'
+              type='text'
+              name='profileImageUrl'
+              className='form-control'
+              autoComplete='off'
+              value={ profileImageUrl }
+              onChange={ this.handleChange } />
+            <button
+              type='submit'
+              className=''
+              >
+              Sign Me Up!
+            </button>
+          </form>
+        </div>
+      );
+    }
+  }
+}
+
+export default AuthForm;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,87 @@
 import React, { Component } from 'react';
+import { Redirect, withRouter } from 'react-router-dom';
+import * as authCalls from './../actions.authApi';
 import avocado from './../images/costa_rica/IMG_8076_avocado.png';
+import AuthForm from '../components/Forms/AuthForm';
 import NavBar from './../components/NavBar';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      fourLetterWord: {},
+      fourLetterWords: [],
+      game: {},
+      games: [],
+      p: '',
+      prefixSuffixRoot: {},
+      prefixSuffixRoots: [],
+      showLoginForm: false,
+      showSignUpForm: false,
+      user: {},
+      user0: {},
+      users: [],
+      verbo: {},
+      verbos: []
+    }
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+  async handleAuth(user) {
+    let currentUser;
+    if (user.username !== '') {
+      currentUser = await authCalls.signUp(user);
+    } else {
+      currentUser = await authCalls.signIn(user);
+    }
+    this.setState({
+      showLoginForm: false,
+      showSignUpForm: false,
+      user: currentUser
+    });
+    if (typeof(Storage) !== "undefined") {
+      localStorage.setItem('user', JSON.stringify(currentUser));
+    } else {
+      console.log('no localStorage');
+    }
+  }
+
+  handleLogOut() {
+    let user = {};
+    if (typeof(Storage) !== 'undefined') {
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+    this.setState({
+      user
+    });
+    this.props.history.push('/');
+  }
   render() {
+    const { showLoginForm, showSignUpForm, user } = this.state;
     return (
       <div className="App">
         <NavBar />
+        { showLoginForm || showSignUpForm ?
+          <AuthForm
+            onAuth={ this.handleAuth }
+            onClose={ () => this.setState({
+              showLoginForm: false,
+              showSignUpForm: false
+            })}
+            onShowLoginForm={ () => this.setState({
+              showLoginForm: true,
+              showSignUpForm: false
+            })}
+            onShowSignUpForm={ () => this.setState({
+              showLoginForm: true,
+              showSignUpForm: false
+            })}
+            showLoginForm={ showLoginForm }
+            showSignUpForm={ showSignUpForm }
+            /> : null
+        }
         <img src={avocado} className="App-logo" alt="logo" />
         <h1 className="App-title">I Do Not Know How this Works</h1>
       </div>
