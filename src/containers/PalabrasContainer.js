87ba8a63c3aff01d288c556c8cad7df483afb82f
Anthony McDonald <a@awews.com>
2018-04-29 23:31:13 -0400

    add Google Analytics

--- src/containers/PalabrasContainer.js
+++ src/containers/PalabrasContainer.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import App from './App';
+
+ReactGA.initialize('UA-10207332-25', {
+  debug: true
+})
+ReactGA.pageview(window.location.ppathname + window.location.search);
 
 const supportsHistory = 'pushState' in window.history;
 
