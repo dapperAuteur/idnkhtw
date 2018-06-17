7e18ed924cef7b82cb9877653b2072fb8d87f258
Anthony McDonald <a@awews.com>
2018-06-17 00:27:52 -0400

    add provider, store, reducer to container

--- src/containers/PalabrasContainer.js
+++ src/containers/PalabrasContainer.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import ReactGA from 'react-ga';
+import authReducer from './../store/reducers/authReducer';
+import { getCurrentUser } from './../store/actions/index';
 import App from './App';
+
+const rootReducer = combineReducers({
+  authReducer
+  // userReducer
+});
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[MIDDLEWARE] Dispatching', action);
+      const result = next(action);
+      console.log('[MIDDLEWARE] next state', store.getState());
+      return result;
+    }
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
+
+if (typeof(Storage) !== "undefined" && localStorage.hasOwnProperty('currentUser')) {
+  let currentUser = store.dispatch(getCurrentUser());
+  console.log(currentUser);
+};
 
 ReactGA.initialize('UA-10207332-25', {
   debug: true
 });
 
-// function logPageView() {
-//   ReactGA.set({ page: window.location.pathname + window.location.search });
-//   ReactGA.pageview(window.location.pathname + window.location.search);
-// }
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 const supportsHistory = 'pushState' in window.history;
@@ -23,10 +49,12 @@ const supportsHistory = 'pushState' in window.history;
 //   </Router>
 // );
 const PalabrasContainer = () => (
-  <Router
-    forceRefresh={ !supportsHistory }>
-    <App />
-  </Router>
+  <Provider store={ store }>
+    <Router
+      forceRefresh={ !supportsHistory }>
+      <App />
+    </Router>
+</Provider>
 );
 
 export default PalabrasContainer;
---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,109 @@
+import * as actionTypes from './../actions/actionTypes';
+
+const initialState = {
+  authenticated: false,
+  currentUser: {},
+  didInvalidate: false,
+  error: false,
+  errorMessage: {},
+  isFetching: false,
+  showLoginForm: false,
+  showSignUpForm: false
+}
+
+const authReducer = (state = initialState, action) => {
+  let currentUser;
+  console.log("authReducer");
+  switch (action.type) {
+    case actionTypes.AUTHENTICATE_USER:
+      console.log("AUTHENTICATE_USER");
+      currentUser = action.currentUser;
+      if (typeof(Storage) !== "undefined") {
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+      } else {
+        return null;
+      }
+      return Object.assign({}, state, {
+        authenticated: true,
+        currentUser,
+        didInvalidate: false,
+        error: false,
+        errorMessage: {},
+        isFetching: false,
+        showLoginForm: false,
+        showSignUpForm: false
+      })
+    case actionTypes.CLOSE_AUTH_FORM:
+      return Object.assign({}, state, {
+        showLoginForm: false,
+        showSignUpForm: false
+      })
+    case actionTypes.SHOW_LOGIN_FORM:
+      return Object.assign({}, state, {
+        showLoginForm: true,
+        showSignUpForm: false
+      })
+    case actionTypes.SHOW_SIGN_UP_FORM:
+      return Object.assign({}, state, {
+        showLoginForm: false,
+        showSignUpForm: true
+      })
+    case actionTypes.GET_CURRENT_USER:
+      console.log('GET_CURRENT_USER');
+      if (typeof(Storage) !== "undefined") {
+        if (localStorage.hasOwnProperty('currentUser')) {
+          currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        }
+      } else {
+        return null;
+      }
+      return Object.assign({}, state, {
+        authenticated: true,
+        currentUser,
+        didInvalidate: false,
+        error: false,
+        errorMessage: {},
+        isFetching: false,
+        showLoginForm: false,
+        showSignUpForm: false
+      })
+    case actionTypes.SET_ERROR:
+      let errorMessage = action.errorMessage;
+      console.log(errorMessage);
+      console.log(action);
+      return Object.assign({}, state, {
+        authenticated: false,
+        currentUser: {},
+        error: true,
+        errorMessage,
+        isFetching: false
+      })
+    case actionTypes.REQUEST_AUTHENTICATION:
+      return Object.assign({}, state, {
+        authenticated: false,
+        error: false,
+        errorMessage: {},
+        isFetching: true
+      })
+    case actionTypes.USER_LOGOUT:
+      console.log("USER_LOGOUT");
+      currentUser = {};
+      if (typeof(Storage) !== "undefined") {
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+      } else {
+        return null;
+      }
+      return Object.assign({}, state, {
+        authenticated: false,
+        currentUser,
+        didInvalidate: false,
+        error: false,
+        errorMessage: {},
+        isFetching: false,
+      })
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
