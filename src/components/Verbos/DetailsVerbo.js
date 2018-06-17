27849188660c8e526db0b502099d6d827c36db20
Anthony McDonald <a@awews.com>
2018-06-17 13:16:01 -0400

    remove if/else statement from detailVerbo component

--- src/components/Verbos/DetailsVerbo.js
+++ src/components/Verbos/DetailsVerbo.js
@@ -22,64 +22,58 @@ const DetailsVerbo = (props) => {
   if (typeof verbo.grupo === "number") {
     grupo = <h3>Group: { verbo.grupo.toString() }</h3>;
   }
-
-  if (verbo === null || verbo === undefined) {
-    verbo = JSON.parse(localStorage.getItem("verbo"));
-    return null;
-  } else {
-    return (
-      <div>
-        <h1>{ verbo.spanish }</h1>
-        <h3>{ verbo.terminación }</h3>
-        { grupo }
-        {
-          showEnglish &&
-          <h3>English: { verbo.english }</h3>
-        }
-        {
-          verbo.reflexive &&
-        <h3 className="reflexive">Reflexive</h3>
-        }
-        {
-          verbo.irregular &&
-          <IrregularCategoria
-            verbo={ verbo } />
-        }
-        {
-          currentUser.userRole === 0 &&
-          <div>
-            <button
-              onClick={ onDeleteVerbo }
-              className="btn btn-danger"
-              >
-              Delete Word
-            </button>
-            <Link
-              to={{
-                pathname: '/words/verbos/update'
-              }}
-              className="btn btn-warning"
-              >
-              EDIT
-            </Link>
-          </div>
-        }
-        <Link
-          to={{
-            pathname: '/words/verbo'
-          }}
-          onClick={ onRandomVerbo }
-          className="btn btn-default">
-          Next Word
-        </Link>
-        <button
-          onClick={ onShowEnglish }
-          className="btn btn-default">
-          Show Translation
-        </button>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>{ verbo.spanish }</h1>
+      <h3>{ verbo.terminación }</h3>
+      { grupo }
+      {
+        showEnglish &&
+        <h3>English: { verbo.english }</h3>
+      }
+      {
+        verbo.reflexive &&
+      <h3 className="reflexive">Reflexive</h3>
+      }
+      {
+        verbo.irregular &&
+        <IrregularCategoria
+          verbo={ verbo } />
+      }
+      {
+        currentUser.userRole === 0 &&
+        <div>
+          <button
+            onClick={ onDeleteVerbo }
+            className="btn btn-danger"
+            >
+            Delete Word
+          </button>
+          <Link
+            to={{
+              pathname: '/words/verbos/update'
+            }}
+            className="btn btn-warning"
+            >
+            EDIT
+          </Link>
+        </div>
+      }
+      <Link
+        to={{
+          pathname: '/words/verbo'
+        }}
+        onClick={ onRandomVerbo }
+        className="btn btn-default">
+        Next Word
+      </Link>
+      <button
+        onClick={ onShowEnglish }
+        className="btn btn-default">
+        Show Translation
+      </button>
+    </div>
+  )
 }
 
 export const mapStateToProps = state => {
