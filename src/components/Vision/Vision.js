51d7536c01adfe11378ba3b1acb336f33ec60940
Anthony McDonald <a@awews.com>
2018-07-31 11:47:57 -0400

    add more text to vision component

--- src/components/Vision/Vision.js
+++ src/components/Vision/Vision.js
@@ -15,6 +15,9 @@ const Vision = () => {
     <div>
       <h1>My Vision</h1>
       <p className="vision">To create things that I can use and will add value to my life.</p>
+      <br />
+      <h4>This is a work in progress. Check back later for updates.</h4>
+      <br />
       <p className="vision">I'd like the opportunity to partner with an organization who's values are in harmony with mine to help them create products that add value to the world.</p>
       <br />
       <p className="vision">This app was created to help me practice Spanish and expand my English vocabulary. It includes a word game called Cows and Bulls that I learned while driving through the mountains of Guatemala during a thunder storm one night. We played the game to eaze our anxiety due to the torrential weather and low visibility.</p>
