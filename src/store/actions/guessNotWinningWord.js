13902d76d6fe3cf7ef0de4ee6d9be2f0f16a3d37
dapperauteur <a@awews.com>
2019-02-04 18:34:09 -0700

    export guessNotWinningWord

---
+++ src/store/actions/guessNotWinningWord.js
@@ -0,0 +1,6 @@
+export const guessNotWinningWord = (currentGame, guessLowerCase) => {
+  let bulls = 0;
+  let cows = 0;
+  let winningWord = currentGame.winningWord;
+  let guess = guessLowerCase;
+};
